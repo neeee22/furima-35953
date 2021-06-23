8a4d9d322cd3615e95b137876405d67ac6e3877f
ninisanni <nee.kenji@gmail.com>
2021-06-23 23:44:04 +0900

    バリデーション、familyをlastに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,22 @@ class User < ApplicationRecord
 
          validates :nickname, presence: true
          validates :first_name, presence: true
-         validates :family_name, presence: true
+         validates :last_name, presence: true
          validates :first_name_kana, presence: true
-         validates :family_name_kana, presence: true
+         validates :last_name_kana, presence: true
          validates :birthday, presence: true
 
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+          validates :first_name
+          validates :last_name
+        end
+        with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カナ文字を使用してください' } do
+          validates :first_name
+          validates :last_name
+        end
          has_many :items
          has_many :purchases
 end
