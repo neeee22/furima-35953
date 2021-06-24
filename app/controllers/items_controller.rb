22ff6fd3b97f33145feb5e600cd77f8fcc50c598
ninisanni <nee.kenji@gmail.com>
2021-06-24 19:11:06 +0900

    active_storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,17 @@ class ItemsController < ApplicationController
   def new
    @item = Item.new
   end
+
+  private 
+  def item_params
+    params.require(:item).permit(:name,
+                                 :price,
+                                 :detail,
+                                 :category_id,
+                                 :status_id,
+                                 :delivery_charge_id,
+                                 :prefectue_id,
+                                 :shipping_date_id,
+                                 :image ).marge(user_id: current_user.id)
+  end
 end
