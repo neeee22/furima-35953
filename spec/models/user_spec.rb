ee1d1b8e22991a1b723a83cfb641ed68ff5b5938
ninisanni <nee.kenji@gmail.com>
2021-06-24 17:43:12 +0900

    レビューを受けて修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,64 +6,70 @@ RSpec.describe User, type: :model do
   end
 
   describe '新規登録/ユーザー情報' do
-    it '全ての値が正しく入力されていれば保存できる' do
-      expect(@user).to be_valid
+
+    context '保存できる場合' do
+      it '全ての値が正しく入力されていれば保存できる' do
+        expect(@user).to be_valid
+      end
+      it 'パスワードは、6文字以上であれば登録できる' do
+        @user.password = 'a11111'
+        @user.password_confirmation = @user.password
+        expect(@user).to be_valid
+      end
     end
-    it 'パスワードは、6文字以上であれば登録できる' do
-      @user.password = 'a11111'
-      @user.password_confirmation = @user.password
-      expect(@user).to be_valid
-    end
-    
-    it 'ニックネームが必須であること' do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname can't be blank")
-    end
-    it 'メールアドレスが必須であること' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
-    end
-    it 'メールアドレスが一意性であること' do
-      @user.save
-      another_user = FactoryBot.build(:user)
-      another_user.email = @user.email
-      another_user.valid?
-      expect(another_user.errors.full_messages).to include("Email has already been taken")
-    end
-    it'メールアドレスは、＠を含む必要があること' do
-      @user.email = "google.com"
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email is invalid")
-    end
-    it 'パスワードが必須であること' do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
-    it 'パスワードは、5文字以下では登録できない' do
-      @user.password = Faker::Internet.password(min_length: 1,max_length: 5)
-      @user.password_confirmation = @user.password
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
-    end
-    it 'パスワードは、数字だけでは登録できない' do
-      @user.password = Faker::Number.number
-      @user.password_confirmation = @user.password
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
-    end
-    it 'パスワードは、英字だけでは登録できない' do
-      @user.password = Faker::Lorem.word
-      @user.password_confirmation = @user.password
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
-    end
-    it 'パスワードとパスワード(確認)は、値の一致が必須であること' do
-      @user.password_confirmation = Faker::Internet.password(min_length: 6) 
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+
+    context '保存できない場合' do
+      it 'ニックネームが必須であること' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'メールアドレスが必須であること' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'メールアドレスが一意性であること' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it'メールアドレスは、＠を含む必要があること' do
+        @user.email = "google.com"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it 'パスワードが必須であること' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      context '保存できない場合' do
+      it 'パスワードは、5文字以下では登録できない' do
+        @user.password = Faker::Internet.password(min_length: 1,max_length: 5)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it 'パスワードは、数字だけでは登録できない' do
+        @user.password = Faker::Number.number
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+      it 'パスワードは、英字だけでは登録できない' do
+        @user.password = Faker::Lorem.word
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+      it 'パスワードとパスワード(確認)は、値の一致が必須であること' do
+        @user.password_confirmation = Faker::Internet.password(min_length: 6) 
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
     end
   end
 
