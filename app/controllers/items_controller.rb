8b4f62c716e187331523f7a180e4c68b4c13c591
ninisanni <nee.kenji@gmail.com>
2021-06-24 18:48:42 +0900

    モデルのバリデーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,6 @@ class ItemsController < ApplicationController
   def index
   end
   def new
-    #@item = Item.new
+   @item = Item.new
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :detail
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :prefectue_id
+    validates :shipping_date_id
+  end
+
+  belongs_to :user
 end
