7b14f0b44aee819b3579a36bd3fddf787cb56ea5
ninisanni <nee.kenji@gmail.com>
2021-06-29 16:52:26 +0900

    コードレビューを受けて修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -47,12 +47,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it '価格が300円以上でない' do
-        @item.price = '100'
+        @item.price = 100
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it '価格が9999999円以下でない' do
-        @item.price = '1000000000'
+        @item.price = 1000000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
@@ -62,22 +62,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it '商品の状態が選択されていない' do
-        @item.status_id = '1'
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it '配送料の負担が選択されていない' do
-        @item.fee_id = '1'
+        @item.fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Fee must be other than 1")
       end
       it '発送元の地域が選択されていない' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it '発送までの日数が選択されていない' do
-        @item.schedule_id = '1'
+        @item.schedule_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Schedule must be other than 1")
       end
