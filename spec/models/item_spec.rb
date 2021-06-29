d6fec52ad4c6d7043fbdef48d5618893a20290e2
ninisanni <nee.kenji@gmail.com>
2021-06-29 16:17:58 +0900

    テストの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -36,6 +36,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
+      it '価格が300円以上でない' do
+        @item.price = '100'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '価格が9999999円以下でない' do
+        @item.price = '1000000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
       it 'カテゴリーが選択されていない' do
         @item.category_id = '1'
         @item.valid?
