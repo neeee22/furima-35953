b13d2af9097ab8838df2caa85d69a38c93895904
ninisanni <nee.kenji@gmail.com>
2021-07-23 05:02:54 +0900

    カードの有無でページの分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,7 @@ class OrdersController < ApplicationController
   before_action :move_to_index
 
   def index
+    redirect_to new_card_path and return unless current_user.card.present?
     @order_address = OrderAddress.new
     show_card
   end
