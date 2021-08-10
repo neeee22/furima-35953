e7d2777a04ec80ac212cf87e0442fad742271e92
ninisanni <nee.kenji@gmail.com>
2021-08-10 20:14:50 +0900

    binding.pry削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = ItemsBrand.new(item_params)
-    binding.pry
     if @item.valid?
       @item.save
       redirect_to root_path
